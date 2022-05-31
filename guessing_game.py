247bb10cd4b9dd9f5137ac9b69e81ed59bee7794
Miah Clay <miahb88@gmail.com>
2022-05-31 16:10:01 -0400

    Solved in Python

--- guessing_game.py
+++ guessing_game.py
@@ -1,23 +1,34 @@
 class GuessingGame():
 
-    def __init__(self, answer=10, user_guess):
-        self.answer = answer
+    def __init__(self, answer):
+        self.answer = int(answer)
+        # print(self.answer)
+    
+    def guess(self, user_guess):
         self.user_guess = user_guess
-        
-    def guess(self):
+        # list = []
+        # list.append(self.user_guess)
         if self.user_guess == self.answer:
             return "Correct!"
         elif self.user_guess > self.answer:
             return "high!"
         else: return "low!"
+
         
     
     def solved(self):
-        self.answer = self.guess()
-        if self.guess() == self.answer:
-            return f"Your last guess {self.user_guess} was {self.answer}."
+        # self.user_guess = user_guess
+        print(list)
+        print(self.user_guess, self.answer)
+        if self.user_guess == self.answer:
+            # return 'True'
+            return f"Your last guess {self.user_guess} was {self.guess(self.user_guess)}"
         else:
-            return f"Your last guess {self.user_guess} was {self.answer}."
+            # return "False"
+            return f"Your last guess {self.user_guess} was {self.guess(self.user_guess)}"
+
+
 
 game = GuessingGame(2)
-print(game(2))
+print(game.guess(1))
+print(game.solved())
---
+++ main2.py
@@ -0,0 +1,2 @@
+name = Miah
+print(f"hello {name}!")
